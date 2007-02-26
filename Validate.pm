7b80efb69f89f75c2c8c2c83131a15715f04beb0
T. Linden <tlinden@cpan.org>
2007-02-26 18:02:26 +0000

    Import of TLINDEN/Config-General-Validate-1.02 from CPAN.
    gitpan-cpan-distribution: Config-General-Validate
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         TLINDEN/Config-General-Validate-1.02.tar.gz
    gitpan-cpan-author:       TLINDEN
    gitpan-cpan-maturity:     released

--- Validate.pm
+++ Validate.pm
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2000-2007 Thomas Linden <tlinden |AT| cpan.org>.
+# Copyright (c) 2007 Thomas Linden <tlinden |AT| cpan.org>.
 # All Rights Reserved. Std. disclaimer applies.
 # Artificial License, same as perl itself. Have fun.
 #
@@ -13,13 +13,19 @@ use Carp;
 use Exporter;
 use Data::Dumper;
 
+use Regexp::Common::URI::RFC2396 qw /$host $port/;
+use Regexp::Common qw /URI/;
+use Regexp::Common qw /net/;
+use File::Spec::Functions qw/file_name_is_absolute/;
+use Regexp::Common qw /delimited/;
+use File::stat;
+
 use constant FALSE => 0;
 use constant TRUE  => 1;
 
-$Config::General::Validate::VERSION = 1.00;
+$Config::General::Validate::VERSION = 1.01;
 
 use vars  qw(@ISA);
-use base qw(Exporter Config::General);
 
 sub new {
   my( $this, $structure ) = @_;
@@ -28,29 +34,72 @@ sub new {
   my $self;
   $self->{structure} = $structure;
   $self->{types} = {
-		    int   => qr(\d+),
-		    word  => qw(\w+),
-		    line  => qw([^\n]+),
-		    text  => qw(.+)
+		    # own
+		    int            => qr(^\d+$),
+		    number         => qr(^[\d\.\-]+$),
+		    word           => qr(^\w+$),
+		    line           => qr/^[^\n]+$/s,
+		    text           => qr/.+/s,
+		    regex          => qr/^qr(.)[^\1]*\1$/,
+
+		    # via imported regexes
+		    uri            => qr(^$RE{URI}$),
+		    cidr           => qr/^$RE{net}{IPv4} \/ ( [89] | [12][0-9] | 3[0-2] )$/x,
+		    ipv4           => qr/^$RE{net}{IPv4}$/,
+                    quoted         => qr/^$RE{delimited}{ -delim => qr(\') }$/,
+		    hostname       => qr(^$host$),
+
+		    # variables according to Config::General::Interpolate
+		    #    $ should not be escaped
+		    #    possible matches: $var ${var} $(var)
+		    novars         => qr/(?<!\\) ( \$\w+ | \$\{[^\}]+\} | \$\([^\)]+\) )/x,
+
+		    # closures
+		    path           => sub { return file_name_is_absolute($_[0]); },
+		    fileexists     => sub { return stat($_[0]); },
+		    resolvablehost => sub { return gethostbyname($_[0]); },
+		    user           => sub { return (getpwnam($_[0]))[0]; },
+		    group          => sub { return getgrnam($_[0]); },
+		    port           => sub { if ( $_[0] =~ /^$port$/ && ($_[0] > 0 && $_[0] < 65535) ) { return 1; } else { return 0; } },
+
 		    };
+
+  $self->{debug} = 0;
 
   bless $self, $class;
 
   return $self;
 }
 
+
+sub type {
+  my ($this, %param) = @_;
+  foreach my $type (keys %param) {
+    $this->{types}->{$type} = $param{$type};
+  }
+}
+
+
+sub debug {
+  my ($this) = @_;
+  $this->{debug} = 1;
+}
+
+sub errstr {
+  my ($this) = @_;
+  if (exists $this->{error}) {
+    return $this->{error};
+  }
+}
+
 sub validate {
   my($this, $config) = @_;
-
-  if (ref($config) eq 'Config::General') {
-    $config = { $config->getall() };
-  }
 
   eval {
     $this->traverse($this->{structure}, $config);
   };
   if ($@) {
-    $! = $@;
+    $this->{error} = $@;
     return FALSE;
   }
   else {
@@ -58,20 +107,28 @@ sub validate {
   }
 }
 
+sub _debug {
+  my ($this, $msg) = @_;
+  if ($this->{debug}) {
+    print STDERR "::Validate::debug() - $msg\n";
+  }
+}
 
 sub traverse {
   my($this, $a, $b) = @_;
 
-  ###print Dumper($a, $b);
-
   foreach my $key (keys %{$a}) {
-
-    ###print "REF $key: " . ref($a->{$key}) . "\n";
 
     if (ref($a->{$key}) eq 'ARRAY') {
       # just use the 1st one, more elements in array are expected to be the same
       foreach my $item (@{$b->{$key}}) {
-	$this->traverse($a->{$key}->[0], $item);
+	if (ref($item) eq q(HASH)) {
+	  $this->traverse($a->{$key}->[0], $item);
+	}
+	else {
+	  # a value, this is tricky
+	  $this->traverse({item => $a->{$key}->[0]}, { item => $item});
+	}
       }
     }
     elsif (ref($a->{$key}) eq 'HASH') {
@@ -84,15 +141,42 @@ sub traverse {
       }
       else {
 	if (exists $b->{$key}) {
-	  if ($b->{$key} !~ /^$this->{types}->{$a->{$key}}$/s) {
-	    die "$key not ok ($b->{$key} !~ /$this->{types}->{$a->{$key}}/)\n";
+	  if (ref($this->{types}->{$a->{$key}}) eq q(CODE)) {
+	    # execute closure
+	    my $sub = $this->{types}->{$a->{$key}};
+	    if (! &$sub($b->{$key})) {
+	      $this->_debug( "$key = $b->{$key}, value is not $a->{$key}");
+	      die "$key = $b->{$key}, value is not $a->{$key}";
+	    }
+	    else {
+	      $this->_debug( "$key = $b->{$key}, value is $a->{$key}");
+	    }
 	  }
 	  else {
-	    ###print "$key ok\n";
+	    if ($a->{$key} =~ /^no/) {
+	      # negative match
+	      if ($b->{$key} =~ /$this->{types}->{$a->{$key}}/) {
+		$this->_debug( "$key = $b->{$key}, value doesn't match /$a->{$key}/");
+		die "$key = $b->{$key}, value matches /$a->{$key}/ which it shouldn't\n";
+	      }
+	      else {
+		$this->_debug( "$key = $b->{$key}, value doesn't match /$a->{$key}/ which is fine");
+	      }
+	    }
+	    else {
+	      # positive match
+	      if ($b->{$key} !~ /$this->{types}->{$a->{$key}}/) {
+		$this->_debug( "$key = $b->{$key}, value doesn't match /$a->{$key}/");
+		die "$key = $b->{$key}, value doesn't match /$a->{$key}/\n";
+	      }
+	      else {
+		$this->_debug( "$key = $b->{$key}, value matches /$a->{$key}/");
+	      }
+	    }
 	  }
 	}
 	else {
-	  die "$key doesn't exist\n";
+	  die "requred $key doesn't exist in hash\n";
 	}
       }
     }
@@ -108,21 +192,12 @@ __END__
 
 =head1 NAME
 
-Config::General::Validate - Validate Config::General Configs
+Config::General::Validate - Validate Config Hashes
 
 =head1 SYNOPSIS
 
  use Config::General::Validate;
- 
  my $validator = new Config::General::Validate($reference);
- 
- if ( $validator->validate($config_general_object) ) {
-   print "valid\n";
- }
- else {
-   print "invalid\n";
- }
- 
  if ( $validator->validate($config_hash_reference) ) {
    print "valid\n";
  }
@@ -132,17 +207,122 @@ Config::General::Validate - Validate Config::General Configs
 
 =head1 DESCRIPTION
 
-This module validates a config parsed by Config::General
-against a given hash structure.
+This module validates a config hash reference
+against a given hash structure. This hash could be the
+result of Config::General or just any other hash structure. Eg. the hash
+returned by L<XML::Simple> could also be validated using
+this module. You may also use it to validate CGI input, just
+fetch the input data from CGI, L<map> it to a hash and
+validate it.
 
-The following data types can be used:
+The following builtin data types can be used:
 
- int  - an integer
- word - a single word
- line - a line of text
- text - a whole text(blob) including newlines.
+=over
 
-Example validator structure:
+=item B<int>
+
+integer
+
+=item B<number>
+
+real number
+
+=item B<word>
+
+single word
+
+=item B<line>
+
+line of text
+
+=item B<text>
+
+a whole text(blob) including newlines.
+
+=item B<regex>
+
+regex starting with qr.
+
+Please note, that this doesn't mean you can provide
+here a regex against config options must match.
+
+Instead this means that the config options contains a regex.
+
+eg:
+
+ <cfg>
+   grp  = qr/root|wheel/
+ </cfg>
+
+B<regex> would match the content of the variable 'grp'
+in this example.
+
+To add your own rules for validation, use the B<type()>
+method, see below.
+
+=item B<uri>
+
+uri, right
+
+=item B<ipv4>
+
+ip address (v4)
+
+=item B<cidr>
+
+same as above with cidr netmast (/24)
+
+=item B<quoted>
+
+text quoted with single quotes
+
+=item B<hostname>
+
+valid hostname
+
+=item B<resolvablehost>
+
+hostname resolvable via dns lookup
+
+=item B<path>
+
+valid absolute path (portable)
+
+=item B<fileexists>
+
+file which must exists
+
+=item B<user>
+
+existent user
+
+=item B<group>
+
+existent group
+
+=item B<port>
+
+valid tcp/udp port
+
+=item B<novars>
+
+text shall not contain variables
+
+=back
+
+
+=head1 VALIDATOR STRUCTURE
+
+The expected structure must be a standard perl hash reference.
+This hash may look like the config you are validating but
+instead of real-live values it contains B<types> that define
+of what type a given value has to be.
+
+In addition the hash may be deeply nested. In this case the
+validated config must be nested the same way as the reference
+hash.
+
+Example:
 
  $reference = { user => 'word', uid => 'int' };
 
@@ -158,25 +338,80 @@ this one not:
                           |                +----- is not a number
                           +---------------------- space not allowed
 
+For easier writing of references you are encouraged to use
+Config::General, just write the definition using the syntax of
+Config::General, get the hash of it and use this hash for
+validation.
+
+See F<t/run.t> there are some examples of this technique.
+
 =head1 SUBROUTINES/METHODS
 
 =over
 
 =item B<validate($config)>
 
-$config can be either a normal hash reference you'd like to
-validate (which allows you to use the module for any hash, not
-only the ones generated by Config::General) or a Config::General
-object.
+$config must be a hash reference you'd like to validate.
 
 It returns a true value if the given structure looks valid.
 
 If the return value is false (0), then the error message will
 be written to the variable $!.
 
+=item B<type(%types)>
+
+You can enhance the validator by adding your own rules. Just
+add one or more new types using a simple hash using the B<type()>
+method. Values in this hash can be regexes or anonymous subs.
+
+Example:
+
+ $v3->type(
+  (
+  address => qr(^\w+\s\s*\d+$),
+  list    =>
+    sub {
+      my $list = $_[0];
+      my @list = split /\s*,\s*/, $list;
+      if (scalar @list > 1) {
+	return 1;
+      }
+      else {
+	return 0;
+      }
+    }
+  )
+ );
+
+In this example we add 2 new types, 'list' and 'address', which
+are really simple. 'address' is a regex which matches a word
+followed by an integer. 'list' is a subroutine which gets called
+during evaluation for each option which you define as type 'list'.
+
+Such subroutines must return a true value in order to produce a match.
+
+You can also add reversive types, which are like all other types
+but start with B<no>. The validator does a negative match in such a
+case, thus you will have a match if a variable does B<not> match
+the type. The builtin type 'novars' is such a type.
+
+Regexes will be executed exactly as given. No flags or ^ or $
+will be used by the module. Eg. if you want to match the whole
+value from beginning to the end, add ^ and $, like you can see
+in our 'address' example above.
+
+=item B<debug()>
+
+Enables debug output which gets printed to STDERR.
+
+=item B<errstr()>
+
+Returns the last error, which is useful to notify the user
+about what happened.
+
 =back
 
-=head1 ARRAY
+=head1 ARRAYS
 
 Arrays must be handled in a special way, just define an array
 with two elements and the second empty. The config will only
@@ -184,7 +419,7 @@ validated against the first element in the array.
 
 We assume all elements in an array must have the same structure.
 
-Example:
+Example for array of hashes:
 
  $reference = {
                 [
@@ -195,6 +430,16 @@ Example:
                   {} # empty 2nd element
                 ]
                };
+
+Example of array of values:
+
+ $reference = {
+  var => [ 'int', '' ]
+ }
+
+=head1 EXAMPLES
+
+Take a look to F<t/run.t> for lots of examples.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
@@ -209,8 +454,8 @@ I recommend you to read the following documentations, which are supplied with pe
  perldsc                        Perl data structures intro
  perllol                        Perl data structures: arrays of arrays
 
- Config::General::Extended      Object oriented interface to parsed configs
- Config::General::Interpolated  Allows to use variables inside config files
+ XML::Simple                    The very same but with xml files.
+
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -229,11 +474,16 @@ None known.
 
 =head1 DIAGNOSTICS
 
-To debug Config::General::Validate use the perl debugger, see L<perldebug>.
+To debug Config::General::Validate use B<debug() or the perl debugger, see L<perldebug>.
+
+For example to debug the regex matching during processing try this:
+
+ perl -Mre=debug yourscript.pl
 
 =head1 DEPENDENCIES
 
-Config::General::Validate depends on the module L<Config::General>.
+Config::General::Validate depends on the module 
+L<Regexp::Common>, L<File::Spec> and L<File::stat>.
 
 =head1 AUTHOR
 
@@ -241,7 +491,7 @@ Thomas Linden <tlinden |AT| cpan.org>
 
 =head1 VERSION
 
-1.00
+1.02
 
 =cut
 
--- t/run.t
+++ t/run.t
@@ -1,56 +1,193 @@
 # -*-perl-*-
 
-use Test::More tests => 2;
+use Test::More tests => 19;
+#use Test::More qw(no_plan);
 
 use lib qw(.);
+
+print STDERR "\n";
 require_ok( 'Config::General::Validate' );
 
-my $struct = {
-	      # hash
-	      'auth' => {
-			 user    => 'word',
-			 uid     => 'int',
-			 comment => 'line'
-			},
+my $refstring = q(
+# test all available types
+v1   int
+v2   number
+v3   word
+v4   line
+v5   text
+v6   hostname
+v7   resolvablehost
+v8   user
+# v9   group  # we do not test group because this is not portable!
+v10  port
+v11  uri
+v12  cidr
+v13  ipv4
+v14  path
+v15  fileexists
+v16  quoted
+v17  regex
+v18  novars
 
-	      # scalar
-	      intro => 'text',
+# test nested structures
+<b1>
+ <b2>
+  <b3>
+   item = int
+  </b3>
+ </b2>
+</b1>
 
-	      # array
-	      'list' => [
-			 {
-			  # reference 1st item in array
-			  left  => 'int',
-			  right => 'int'
-			 },
-			 {
-			  # empty 2nd item
-			 }
-			]
+# list
+item number
+item        # can be empty
+);
 
-	      };
-
-my $cfg = new Config::General(
-  -String => q(
-<auth>
- user root
- uid  9
- comment hans dampf
-</auth>
-intro <<EOF
-this is
-an intro
-text.
+my $positivestring = q(
+v1   1234
+v2   19.03
+v3   Johannes
+v4   this is a line of text
+v5   <<EOF
+  This is a text block
+  This is a text block
 EOF
-<list>
-  left 1
-  right 2
-</list>
-<list>
-  left 2
-  right 4
-</list>
-));
+v6   search.cpan.org
+v7   search.cpan.org
+v8   root
+v10  22
+v11  http://search.cpan.org/~tlinden/?ignore&not=1
+v12  192.168.1.101/18
+v13  10.0.0.193
+v14  /etc/ssh/sshd.conf
+v15  MANIFEST
+v16  ' this is a quoted string '
+v17  qr/[0-9]+/
+v18  Doesnt contain any variables
+<b1>
+ <b2>
+  <b3>
+   item = 100
+  </b3>
+ </b2>
+</b1>
+item 10
+item 20
+item 30
+);
 
-my $v = new Config::General::Validate($struct);
-ok ($v->validate($cfg), "validate a reference against a config $!")
+
+my $refcfg = new Config::General(-String => $refstring);
+my $ref    = { $refcfg->getall() };
+
+my $cfg    = new Config::General(-String => $positivestring);
+
+my $v = new Config::General::Validate($ref);
+ok ($v->validate($cfg), "validate a reference against a config " . $v->errstr());
+
+
+
+# check failure matching
+my @failure =
+(
+ { cfg  => q(acht),
+   type => q(int)
+ },
+
+ { cfg  => q(27^8),
+   type => q(number)
+ },
+
+ { cfg  => q(two words),
+   type => q(word)
+ },
+
+ { cfg  => qq(<<EOF\nzeile1\nzeile2\nzeile3\nEOF\n),
+   type => q(line)
+ },
+
+ { cfg  => q(ätz),
+   type => q(hostname)
+ },
+
+ { cfg  => q(gibtsnet123456790.intern),
+   type => q(resolvablehost)
+ },
+
+ { cfg  => q(äüö),
+   type => q(user)
+ },
+
+ { cfg  => q(äüö),
+   type => q(group)
+ },
+
+ { cfg  => q(234234444),
+   type => q(port)
+ },
+
+ { cfg  => q(unknown:/unsinnüäö),
+   type => q(uri)
+ },
+
+ { cfg  => q(1.1.1.1/33),
+   type => q(cidr)
+ },
+
+ { cfg  => q(300.1.1.1),
+   type => q(ipv4)
+ },
+
+ { cfg  => q(üäö),
+   type => q(fileexists)
+ },
+
+ { cfg  => q(not quoted),
+   type => q(quoted)
+ },
+
+ { cfg  => q(no regex),
+   type => q(regex)
+ },
+
+ { cfg  => q($contains some $vars),
+   type => q(novars)
+ }
+);
+
+foreach my $test (@failure) {
+  my $refcfg = new Config::General(-String => q(v = ) . $test->{type});
+  my $ref    = { $refcfg->getall() };
+  my $cfg    = new Config::General(-String => q(v = ) . $test->{cfg});
+  my $v      = new Config::General::Validate($ref);
+  if ($v->validate($cfg)) {
+    fail("could not catch invalid \"$test->{type}\"");
+  }
+  else {
+    pass ("catched invalid \"$test->{type}\"");
+  }
+}
+
+
+
+# adding custom type
+my $ref3 = { v1 => 'address', v2 => 'list'};
+my $cfg3 = { v1 => 'Marblestreet 15', v2 => 'a1, b2, b3'};
+my $v3   = new Config::General::Validate($ref3);
+$v3->type(
+ (
+  address => qr(^\w+\s\s*\d+$),
+  list    =>
+    sub {
+      my $list = $_[0];
+      my @list = split /\s*,\s*/, $list;
+      if (scalar @list > 1) {
+	return 1;
+      }
+      else {
+	return 0;
+      }
+    }
+ )
+);
+ok($v3->validate($cfg3), "using custom types" . $v3->errstr());
